542c8b2c59aa74b209b0fff128ea38fc0711d1d3
ppsegur <pepesegurarodriguez@gmail.com>
2025-01-23 14:20:04 +0100

    Apuntes de consultas dentro del repo

--- herencia/src/main/java/com/salesianostriana/dam/herencia/repo/FacturaRepositorio.java
+++ herencia/src/main/java/com/salesianostriana/dam/herencia/repo/FacturaRepositorio.java
@@ -3,6 +3,18 @@ package com.salesianostriana.dam.herencia.repo;
 import com.salesianostriana.dam.herencia.modelo.Factura;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FacturaRepositorio extends
         JpaRepository<Factura, Long> {
+
+    //Select * from Factura where numero like ="numero"
+    List<Factura> findByNumero(String numero);
+    //List<Factura> == tipo de retorno
+    //FindBy == sujeto
+    //Lo que sea despues del find by sera el predicado y lo que definira
+    //los ajustes de busquedad
+
+    //Devolverá las 10 facturas mas baratas
+    List<Factura> findTop10ByOrderByImporteDesc();
 }
